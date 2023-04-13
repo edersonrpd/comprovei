a96409a054114b920946ace1dc5f3befad60f2c2
Ederson Roberto Domiciano <edersonrp@gmail.com>
2023-04-13 18:16:45 -0300

    Inserido constantes para os diretorios

--- comprovei_dia_atual.py
+++ comprovei_dia_atual.py
@@ -11,12 +11,18 @@ import logging
 import datetime
 from datetime import datetime, timedelta
 from pathlib import Path
-from dotenv import dotenv_values , load_dotenv
+from dotenv import dotenv_values, load_dotenv
 
+# Carregando informações de username e password de um arquivo externo.
 load_dotenv()
 config = dotenv_values("config.env")
 username = config['USERNAME']
 password = config["PASSWORD"]
+
+DATA_DIR = 'C:\ComproveiSAC'
+DATA_EXTRACTION_DIR = os.path.join(DATA_DIR, 'extraidos')
+CSV_OUTPUT_FILE = os.path.join(DATA_DIR, 'dados.csv')
+EXCEL_OUTPUT_FILE = os.path.join(DATA_DIR, 'dados.xlsx')
 
 # Configure o registro
 logging.basicConfig(filename='Log.log', level=logging.INFO,
@@ -179,74 +185,78 @@ arquivo_saida = 'C:\ComproveiSAC\dados.csv'
 # Substitua pelo nome do arquivo de saída
 arquivo_saida_excel = 'C:\ComproveiSAC\dados.xlsx'
 
+
 # Array para armazenar o csv concatenado
 lista_dfs = []
-
 # Lista de arquivos no diretório ordenados por data de criação
 arquivos = sorted(Path(dir_csv).glob('*.csv'))
-
 tipos_colunas = {
-'Documento': str,
-'CNPJ Embarcador': str,
-'CNPJ Cliente': str,
-'CNPJ Transp.': str,
-'Status': str,
-'Modelo': str,
-'Gerente Cód.' : str,
-'Gerente Nome': str,
-'Gerente Emai': str,
-'Gerente Tel.' : str,
-'Supervisor Cód.' : str,
-'Supervisor Nome' : str,
-'Supervisor Email' : str,
-'Supervisor Tel.' : str,
-'Gerente Sênior Cód.' : str,
-'Gerente Sênior Nome' : str,
-'Gerente Sênior Email' : str,
-'Gerente Sênior Tel.' : str,
-'Vendedor Cód.' : str,
-'Vendedor Nome' : str,
-'Vendedor Email' : str,
-'Vendedor Tel.' : str,
-'Pedido' : str,
-'AWB': str,
-'Remessa': str
+    'Documento': str,
+    'CNPJ Embarcador': str,
+    'CNPJ Cliente': str,
+    'CNPJ Transp.': str,
+    'Status': str,
+    'Modelo': str,
+    'Gerente Cód.': str,
+    'Gerente Nome': str,
+    'Gerente Emai': str,
+    'Gerente Tel.': str,
+    'Supervisor Cód.': str,
+    'Supervisor Nome': str,
+    'Supervisor Email': str,
+    'Supervisor Tel.': str,
+    'Gerente Sênior Cód.': str,
+    'Gerente Sênior Nome': str,
+    'Gerente Sênior Email': str,
+    'Gerente Sênior Tel.': str,
+    'Vendedor Cód.': str,
+    'Vendedor Nome': str,
+    'Vendedor Email': str,
+    'Vendedor Tel.': str,
+    'Pedido': str,
+    'AWB': str,
+    'Remessa': str
 }
 
 for arquivo in arquivos:
-    filename = arquivo.name
-    if filename != 'dados.csv':
-        # Ler o arquivo csv e armazenar em um DataFrame
-        df = pd.read_csv(os.path.join(dir_csv, filename), dtype=tipos_colunas, parse_dates=[
-                         'Emissão', 'Data Finalização', 'Data Pagamento'])
+        filename = arquivo.name
+        if filename != 'dados.csv':
+            # Ler o arquivo csv e armazenar em um DataFrame
+            df = pd.read_csv(os.path.join(dir_csv, filename),
+                             dtype=tipos_colunas, low_memory=False)
 
-        # Adicionar o DataFrame à lista
-        lista_dfs.append(df)
+            # Adicionar o DataFrame à lista
+            lista_dfs.append(df)
 
-
-# Concatenar todos os DataFrames na lista
+    # Concatenar todos os DataFrames na lista
 df_concatenado = pd.concat(lista_dfs, ignore_index=True)
-
 
 # Excluindo linhas duplicadas
 df_concatenado = df_concatenado.drop_duplicates()
-#df_concatenado = df_concatenado.sort_values(by=['Emissão'], ascending=False)
+    # df_concatenado = df_concatenado.sort_values(by=['Emissão'], ascending=False)
 print("Arquivos CSV concatenados com sucesso!")
 
-# Alterando o type de algumas colunas
+    # Alterando o type de algumas colunas
 colunas = ['Pedido', 'CNPJ Embarcador', 'CNPJ Cliente', 'CNPJ Transp.']
 
 for coluna in colunas:
     df_concatenado[coluna] = df_concatenado[coluna].astype(pd.Int64Dtype())
 
 # Excluindo elementos duplicados e mantendo apenas ultimo registro
-df_concatenado = (df_concatenado.sort_index()
-                  .drop_duplicates(
-                      subset=['Documento', 'CNPJ Cliente'], keep='last')
-                  .sort_values(by=['Emissão'], ascending=False))
 
+df_concatenado = (df_concatenado.sort_index()
+                      .drop_duplicates(
+                          subset=['Documento', 'CNPJ Cliente'], keep='last')
+                      .sort_values(by=['Emissão'], ascending=False))
 
 # Salvar o arquivo CSV concatenado
-df_concatenado.to_csv(arquivo_saida, index=False, sep=';')
-logging.info(f'Arquivo {arquivo_saida} salvo com sucesso')
-df_concatenado.to_excel(arquivo_saida_excel, index=False)
+
+
+def save_output(df_concatenado):
+    df_concatenado.to_csv(CSV_OUTPUT_FILE, index=False, sep=';')
+    logging.info(f'Arquivo {CSV_OUTPUT_FILE} salvo com sucesso')
+    df_concatenado.to_excel(EXCEL_OUTPUT_FILE, index=False)
+
+
+if __name__ == '__main__':
+    save_output(df_concatenado)
